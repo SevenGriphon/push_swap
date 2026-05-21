bef461ff7de14c061e58a35bbe2f540b5480d09d
alnoviko <alnoviko@f6r8s15.paris.42.school>
2026-05-21 16:27:24 +0200

    Implement some libft functions in main

--- main.c
+++ main.c
@@ -1,42 +1,5 @@
 #include "push_swap.h"
 
-static int	ft_isspace(char c)
-{
-	if ((c >= 9 && c <= 13) || c == ' ')
-	{
-		return (1);
-	}
-	return (0);
-}
-
-int	ft_atoi(const char *nptr)
-{
-	int	result;
-	int	sign;
-
-	result = 0;
-	sign = 1;
-	while (ft_isspace(*nptr) == 1)
-	{
-		nptr++;
-	}
-	if (*nptr == '+' || *nptr == '-')
-	{
-		if (*nptr == '-')
-		{
-			sign = -1;
-		}
-		nptr++;
-	}
-	while (*nptr >= '0' && *nptr <= '9')
-	{
-		result = result * 10 + *nptr - '0';
-		nptr++;
-	}
-	result *= sign;
-	return (result);
-}
-
 void print_stack(t_list *stack)
 {
     while (stack)
@@ -56,11 +19,9 @@ t_list *create_node(char *arg)
     int *value;
     t_list *node;
 
-    node = (t_list *)malloc(sizeof(t_list));
-    node->next = NULL;
     value = malloc(sizeof(int));
     *value = ft_atoi(arg);
-    node->content = value;
+    node = ft_lstnew(value);
     return (node);
 }
 
@@ -68,8 +29,6 @@ int main(int argc, char **argv)
 {
     int i;
     t_list *a;
-    t_list *previous_node;
-    t_list *node;
 
     if (argc < 2)
     {
@@ -78,13 +37,10 @@ int main(int argc, char **argv)
 
     i = 1;
     a = create_node(argv[i]);
-    previous_node = a;
     i++;
     while (i < argc)
     {
-        node = create_node(argv[i]);
-        previous_node->next = node;
-        previous_node = node;
+        ft_lstadd_back(&a, create_node(argv[i]));
         i++;
     }
     print_stack(a);
